dac0753851f4c6f6bd91240ae3eafdc0343161ef
Abhay Pawar <abhaypawar924@gmail.com>
2024-10-03 03:12:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://complexity-client.vercel.app/', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+}));
 app.use(express.json());
 
 app.use('/api/v1/user', signup);
